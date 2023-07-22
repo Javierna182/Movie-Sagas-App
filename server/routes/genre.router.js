f0bde50ea4b768c8923e0513f57a7bd5136f554d
Javier Navarro <javierna182@gmail.com>
2023-07-21 21:12:33 -0500

    basic set up and genre router get set up

--- server/routes/genre.router.js
+++ server/routes/genre.router.js
@@ -4,7 +4,25 @@ const pool = require('../modules/pool')
 
 router.get('/', (req, res) => {
   // Add query to get all genres
-  res.sendStatus(500)
+  const queryText = "SELECT * FROM movie JOIN movies_genres ON movie.id = movies_genres.movie_id JOIN  genres ON genres.id = movies_genres.genre_id;";
+  pool
+     .query(queryText)
+     .then((result) => {
+       res.send(result.rows);
+     })
+     .catch((err) => {
+      console.log("Error completing SELECT class query", err);
+      res.sendStatus(500)
+    });
+  // res.sendStatus(500)
 });
 
 module.exports = router;
+
+// SELECT * FROM movie
+// JOIN movies_genres ON movie.id = movies_genres.movie_id
+// JOIN  genres ON genres.id = movies_genres.genre_id;
+
+// SELECT * FROM Person
+// JOIN personHobby ON person.id = PersonHobby.person_id
+// JOIN  Hobby ON Hobby.id = personHobby.hobby_id;
